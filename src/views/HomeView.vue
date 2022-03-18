7870c675f79464039331e310920fdb2ef9002f49
ValentinaCerda <valentinacerda.a@gmail.com>
2022-03-17 22:07:30 -0300

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,28 @@
+<template>
+  <h1 class="mt-5">Letra de Canciones</h1>
+
+  <div class="container">
+    <div class="row">
+      <div class="col-6">
+        <form>
+          <div class="mb-3">
+            <label for="artista" class="form-label">Artista</label>
+            <input type="text" class="form-control" required>
+          </div>
+          <div class="mb-3">
+            <label for="cancion" class="form-label">Canción</label>
+            <input type="text" class="form-control" required>
+          </div>
+          <div class="d-grip gap-2">
+            <router-link :to="`/LyricsView/${lyrics}`"  class="btn btn-success">Buscar</router-link>
+          </div>
+        </form>
+      </div>
+      
+    </div>
+  </div>
+</template>
+
+<script>
+
+</script>
---
+++ src/views/LyricsView.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="result" v-for="lyric of lyrics" :key="lyric">
+        <h2 class="text-center mb-2">{{artist}}</h2>
+        <div class="mt-4">{{lyrics}}</div>
+      </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      lyric: '',
+    };
+  },
+  methods: {
+    async obtenerLyrics() {
+      try {
+        const data = await fecth(`https://api.lyrics.ovh/v1/${this.artist}/${this.lyrics}`);
+        const getLyrics = await data.json();
+        this.lyrics = getLyrics.results;
+      } catch (error) {
+        throw error;
+      }
+    },
+  },
+  created() {
+    this.obtenerLyrics();
+  }
+}
+</script>
+
+<style>
+
+</style>
